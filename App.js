843f9af5650ef181b9d5f0aea89a5528948ec464
aaquib64 <aaquibshaikh64@gmail.com>
2025-03-15 13:43:49 +0530

    Success

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 const express = require("express");
 // const routes = require("./Routes/");
-const routes = require("./routes/");
+const routes = require("./routes/Index.js");
 
 const app = express();
 const port = 5408;
