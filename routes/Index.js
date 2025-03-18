20bd1429458931147dd8d671b2c81cb1703ebced
aaquib64 <aaquibshaikh64@gmail.com>
2025-03-19 01:10:22 +0530

    change

--- routes/Index.js
+++ routes/Index.js
@@ -1,15 +1,15 @@
 //create some routes which can recieve the incoming reuest
 
-const express = require("express");    // Import express package
-const router = express.Router();       // Import Router from express package
+const express = require("express"); // Import express package
+const router = express.Router(); // Import Router from express package
 
-const controller = require("../Controller/Index");     // Import controller
+const controller = require("../Controller/Index"); // Import controller
 
 router.get("/products", controller.getAllProducts);
 
-router.get("/products/:names", controller.getAllProductstByName);
+router.get("/gender/:names", controller.getAllProductstByName);
 
-router.get("/getAllRestaurantByGender/:gender", controller.getAllRestaurantByGender);
+router.get("/:gender/:category", controller.getAllRestaurantByGender);
 
 //router.get('/getMealTypes', controller.getAllMealTypes);
 
