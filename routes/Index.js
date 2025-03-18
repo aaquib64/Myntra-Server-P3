89fde78a55ccd5853bc1eb232ed4f6fb8ee20ad7
aaquib64 <aaquibshaikh64@gmail.com>
2025-03-18 23:58:28 +0530

    update

--- routes/Index.js
+++ routes/Index.js
@@ -7,9 +7,9 @@ const controller = require("../Controller/Index");     // Import controller
 
 router.get("/products", controller.getAllProducts);
 
-router.get("/products/:names", controller.getAllProductstByName);
+router.get("/:gender/:names", controller.getAllProductstByName);
 
-router.get("/getAllRestaurantByGender/:gender", controller.getAllRestaurantByGender);
+router.get("/:gender", controller.getAllRestaurantByGender);
 
 //router.get('/getMealTypes', controller.getAllMealTypes);
 
